49d446296f17cbece0b7f6fbd42f43a539cded8f
Nguyen Minh Tuan <tnowad@gmail.com>
2024-11-29 00:15:50 +0700

    feat: add color

--- lua/mono-colors/colors.lua
+++ lua/mono-colors/colors.lua
@@ -1,41 +1,48 @@
 local colors = {
-	white = "#ffffff",
-	black = "#1e1e1e",
+	-- Text colors
+	text_primary = "#B0B0B0", -- Gray 6
+	text_secondary = "#A0A0A0", -- Gray 7
+	text_highlight = "#808080", -- Gray 5
+	text_cursor = "#D3D3D3", -- Gray 3
+	text_cursor_highlight = "#C0C0C0", -- Gray 4
 
-	-- background colors
-	bg = "#000000",
-	bgDark = "#000000",
-	bgDarker = "#121212",
-	bgFloat = "#212121",
-	bgOption = "#121212",
-	-- foreground colors,
-	fg = "#bababa",
-	fgAlt = "#fafafa",
-	fgCommand = "#dedede",
-	fgInactive = "#333333",
-	fgDisabled = "#787878",
-	fgLineNr = "#616161",
-	fgSelection = "#e8e8e8",
-	fgSelectionInactive = "#f5f5f5",
-	-- border colors,
-	border = "#121212",
-	borderFocus = "#303030",
-	borderDarker = "#1f1f1f",
-	-- ui colors,
-	red = "#999999",
-	blue = "#a3a3a3",
-	orange = "#9c9c9c",
-	purple = "#FFFFFF",
-	blueLight = "#a3a3a3",
-	orangeLight = "#b5b5b5",
-	yellowDark = "#919191",
-	green = "#4c4c4c",
-	redDark = "#4c4c4c",
-	purpleDark = "#1c1c1c",
-	comment = "#616161",
-	symbol = "#b2b2b2",
-	primary = "#ffffff",
-	terminalBrightBlack = "#4a4a4a",
+	-- Background colors
+	background_primary = "#000000", -- High contrast black background
+	background_secondary = "#1A1A1A", -- Darker gray for secondary background
+
+	-- UI container colors
+	ui_container_primary = "#4C4C4C", -- Gray 4
+	ui_container_secondary = "#595959", -- Gray 5
+	ui_border = "#6A6A6A", -- Gray 6
+	ui_selection = "#7A7A7A", -- Gray 7
+	ui_separator = "#8A8A8A", -- Gray 8
+
+	-- Message colors
+	messages_error = "#FF0000", -- Red for error messages
+	messages_warning = "#FFA500", -- Orange for warnings
+	messages_info = "#1E90FF", -- Blue for informational messages
+	messages_success = "#32CD32", -- Green for success messages
+
+	-- Diagnostics colors
+	diagnostics_text = "#9E9E9E", -- Gray 8 for diagnostics text
+
+	-- Syntax colors
+	syntax_literal_string = "#CCCCCC", -- Gray 9 for strings
+	syntax_literal_number = "#D1D1D1", -- Gray 8 for numbers
+	syntax_literal_boolean = "#A8A8A8", -- Gray 7 for booleans
+
+	syntax_type_type = "#7A7A7A", -- Gray 6 for types
+	syntax_type_class = "#6A6A6A", -- Gray 5 for classes
+	syntax_type_struct = "#5A5A5A", -- Gray 4 for structs
+
+	syntax_keyword = "#444444", -- Gray 3 for keywords
+	syntax_operator = "#3E3E3E", -- Gray 2 for operators
+	syntax_identifier = "#B0B0B0", -- Gray 6 for identifiers
+
+	-- Error colors
+	error_critical = "#B22222", -- Firebrick red for critical errors
+	error_warning = "#FFA500", -- Orange for warnings
+	error_info = "#1E90FF", -- Dodger blue for info
 }
 
 return colors
